6d962422e2b90ea31f67b58f1aeba8aacd87bd23
dohyeon1996 <sdh30113@naver.com>
2021-04-22 09:46:43 +0900

    4월22일1교시

--- src/chat7/MultiServer.java
+++ src/chat7/MultiServer.java
@@ -53,6 +53,7 @@ public class MultiServer {
 				//내부클래스의 객체 생성 및 쓰레드시작
 				//클라이언트 한명당 하나씩의 쓰레드가 생성된다.
 				Thread mst = new MultiServerT(socket);
+				
 				mst.start();
 			}
 		}
@@ -120,7 +121,7 @@ public class MultiServer {
 				/*
 				방금 접속한 클라이언트를 제외한 나머지에게 입장을 알린다. 
 				 */
-				sendAllMsg(" ", name+"님이입장하셨습니다.");
+				sendAllMsg(" ", name+"님이입장하셨습니다.","All");
 				//현재 접속한 클라이언트르 HashMap에 저장한다.
 				clientMap.put(name, out);
 				//접속자의 이름을 서버에 콘솔에 띄워주고
@@ -134,9 +135,21 @@ public class MultiServer {
 					if(s==null)
 						break;
 					System.out.println(name+">>"+s);
-					sendAllMsg(name, s);
+					//클라이언트 측으로 전송한다. 
+					if(s.charAt(0)=='/') {
+						String[] strArr=s.split(" ");
+						String msgContent="";
+						for(int i=2;i<strArr.length;i++) {
+							msgContent +=strArr[i]+" ";
+						}
+						if(strArr[0].equals("/to")) {
+							sendAllMsg(strArr[1], msgContent,"One");
+						}
+					}
+					else {
+						sendAllMsg(name, s,"All");
+					}
 				}
-				
 			} 
 			catch (Exception e) {
 				System.out.println("예외:"+ e);
@@ -147,7 +160,7 @@ public class MultiServer {
 				finally절로 진입하게된다. 이때 "대화명"을 통해 정보를 삭제한다. 
 				 */
 				clientMap.remove(name);
-				sendAllMsg("", name+"님이퇴장하셨습니다.");
+				sendAllMsg("", name+"님이퇴장하셨습니다.","All");
 				System.out.println(name+" ["+Thread.currentThread().getName()+"]퇴장");
 				System.out.println("현재 접속자수는"+clientMap.size()+"명입니다");
 
@@ -162,28 +175,41 @@ public class MultiServer {
 			}
 		}
 		//접속된 모든 클라이언트 측으로 서버의 메세지를 Echo해주는 역활담당
-		public void sendAllMsg(String name, String msg)
+		public void sendAllMsg(String name, String msg, String flag)
 		{
 			//Map에 저장된 객체의 키값(대화명)을 먼저얻어온다. 
 			Iterator<String> it=clientMap.keySet().iterator();
 			//저장된 객체(클라이언트)의 갯수만큼 반복한다. 
 			while(it.hasNext()) {
 				try {
+					//컬렉션의 Key는 클라이언트대화명이다. 
+					String clientName=it.next();
 					//각 클라이언트의 PrintWriter  객체를 얻어온다. 
 					PrintWriter it_out=(PrintWriter)
-							clientMap.get(it.next());
+							clientMap.get(clientName);
+					if(flag.equals("One")) {
+						//flag가 One이면 해당클라이언트 한명에게만전송한다.(귓속말)
+						if(name.equals(clientName)) {
+							//컬렉션에 저장된 접속자명과 일치하는 경우에만 메세지를전송한다.
+							it_out.println("[귓속말]"+msg);
+						}
+						
+					}
+					else {
+						//그외에는 모든클라이언트에게 전송한다. 
+						if(name.equals("")) {
+							//입장,퇴장에서 사용되는 부분
+							it_out.println(msg);
+						}
+						else {
+							//메세지를 보낼때사용되는 부분
+							it_out.println("["+name+"]"+msg);
+						}
+					}
 					/*
 					클라이언트에게 메세지를 전달할때 매개변수로 name이
 					있는경우와 없는경우를 구분해서 전달하게 된다. 
 					 */
-					if(name.equals("")) {
-						//입장,퇴장에서 사용되는 부분
-						it_out.println(msg);
-					}
-					else {
-						//메세지를 보낼때사용되는 부분
-						it_out.println("["+name+"]"+msg);
-					}
 				} catch (Exception e) {
 					// TODO: handle exception
 				}
